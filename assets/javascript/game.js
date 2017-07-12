4c191e5efdab810e5a9055dd4af4303cee2c6c56
Tabitha Love <tabinylove@Tabithas-MacBook-Pro.local>
2017-07-12 14:37:35 -0600

    needs condensing and second game bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 
 // VARIABLES
 
-	var randomNum; // random number to be guessed
-	var showRandomNum; // randomNum in html
-	var userNum = 0; // number the user adds together to reach random number
-	var showUserNum; // userNum in html
-	var randomValue; // random value assigned to the buttons
-	var valueArray = []; // when buttons are pushed, they are added to here
-	var parseValue;
+	var randomNum; // random number to be guessed (between 19-120)
+	var userNum; // number the user adds together to reach random number
+	var randomValue; // random value assigned to the buttons (between 1-12)
+	var parseValue; // the 'randomValue' in integer form
+	var wins = 0; // number of wins
+	var losses = 0; // number of losses
 //===============================================================================
 
 //RANDOM NUMBER
@@ -20,7 +19,18 @@ $(document).ready(function(){
 		console.log(randomNum);
 
 		// show it in html
-		showRandomNum = $('#randomNum').html(randomNum);
+		$('#randomNum').html(randomNum);
+
+//===============================================================================
+
+// USER NUMBER
+
+// user number will start out as zero
+	userNum = 0;
+
+		// show it in html
+		$('#userNum').html(userNum);
+
 //===============================================================================
 
 // BUTTONS
@@ -31,20 +41,50 @@ to determine the value of the crystals.*/
 	randomValue = Math.floor(Math.random() * (10 - 1) + 1);
 
 /*-------This ^^^ and the buttons are repeated so that randomValue will NOT be set as the
-same number for all buttons. (i.e. '.btn' instead of '#gem1')-------*/
+same number for all buttons. (i.e. don't use '.btn' instead of '#gem1', ect.)-------*/
 
 	// FIRST
 	// set the value attribute on the buttons as the randomValue
 	$("#gem1").attr("value", randomValue);
 	console.log("#1: " + randomValue);
 
-		// on click, parse the value and send it to 'valueArray'
-		$("#gem1").on("click", function() {
+		// on click, parse the value and add it to 'userNum'
+		$("#gem1").on("click", function gem1() {
 				// parseInt the value
 				parseValue = parseInt($(this).attr("value"));
-				// send to array
-				valueArray.push(parseValue);
-				console.log(valueArray);
+				// add the value to the userNum
+				userNum = userNum + parseValue;
+				console.log(userNum);
+				// show the result in html
+				$('#userNum').html(userNum);
+
+				// WIN / LOSE
+
+				// if the user's number matches the random number, add 1 point to wins in html
+				if (userNum == randomNum) {
+						$('#userNum').html(userNum);
+						wins++;
+						$('#wins').html("Wins: " + wins);
+						// alert
+						alert("You Win!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+
+				// if user's number is greater than random number, add 1 point to wins in html
+				} else if (userNum > randomNum) {
+						$('#userNum').html(userNum);
+						losses++;
+						$('#losses').html("Losses: " + losses);
+						// alert
+						alert("You Lose!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+				}
+
 		})
 
 	// SECOND
@@ -53,13 +93,42 @@ same number for all buttons. (i.e. '.btn' instead of '#gem1')-------*/
 	$("#gem2").attr("value", randomValue);
 	console.log("#2: " + randomValue);
 
-		// on click, parse the value and send it to 'valueArray'
-			$("#gem2").on("click", function() {
+		// on click, parse the value and add it to 'userNum'
+			$("#gem2").on("click", function gem2() {
 				// parseInt the value
 				parseValue = parseInt($(this).attr("value"));
-				// send to array
-				valueArray.push(parseValue);
-				console.log(valueArray);
+				// add the value to the userNum
+				userNum = userNum + parseValue;
+				console.log(userNum);
+				// show the result in html
+				$('#userNum').html(userNum);
+
+				// WIN / LOSE
+
+				// if the user's number matches the random number, add 1 point to wins in html
+				if (userNum == randomNum) {
+						$('#userNum').html(userNum);
+						wins++;
+						$('#wins').html("Wins: " + wins);
+						// alert
+						alert("You Win!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+
+				// if user's number is greater than random number, add 1 point to wins in html
+				} else if (userNum > randomNum) {
+						$('#userNum').html(userNum);
+						losses++;
+						$('#losses').html("Losses: " + losses);
+						// alert
+						alert("You Lose!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+				}
 		})
 
 	// THIRD
@@ -68,13 +137,42 @@ same number for all buttons. (i.e. '.btn' instead of '#gem1')-------*/
 	$("#gem3").attr("value", randomValue);
 	console.log("#3: " + randomValue);
 
-		// on click, parse the value and send it to 'valueArray'
-			$("#gem3").on("click", function() {
+		// on click, parse the value and add it to 'userNum'
+			$("#gem3").on("click", function gem3() {
 				// parseInt the value
 				parseValue = parseInt($(this).attr("value"));
-				// send to array
-				valueArray.push(parseValue);
-				console.log(valueArray);
+				// add the value to the userNum
+				userNum = userNum + parseValue;
+				console.log(userNum);
+				// show the result in html
+				$('#userNum').html(userNum);
+
+				// WIN / LOSE
+
+				// if the user's number matches the random number, add 1 point to wins in html
+				if (userNum == randomNum) {
+						$('#userNum').html(userNum);
+						wins++;
+						$('#wins').html("Wins: " + wins);
+						// alert
+						alert("You Win!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+
+				// if user's number is greater than random number, add 1 point to wins in html
+				} else if (userNum > randomNum) {
+						$('#userNum').html(userNum);
+						losses++;
+						$('#losses').html("Losses: " + losses);
+						// alert
+						alert("You Lose!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+				}
 		})
 
 	// FOURTH
@@ -83,29 +181,44 @@ same number for all buttons. (i.e. '.btn' instead of '#gem1')-------*/
 	$("#gem4").attr("value", randomValue);
 	console.log("#4: " + randomValue);
 
-		// on click, parse the value and send it to 'valueArray'
-			$("#gem4").on("click", function() {
+		// on click, parse the value and add it to 'userNum'
+			$("#gem4").on("click", function gem4() {
 				// parseInt the value
 				parseValue = parseInt($(this).attr("value"));
-				// send to array
-				valueArray.push(parseValue);
-				console.log(valueArray);
+				// add the value to the userNum
+				userNum = userNum + parseValue;
+				console.log(userNum);
+				// show the result in html
+				$('#userNum').html(userNum);
 
-				// for (var i = 0; i < valueArray.length; i++) {
-    // 			userNum += valueArray[i]; 
-    // 			console.log(userNum);
-				// }
+				// WIN / LOSE
+
+				// if the user's number matches the random number, add 1 point to wins in html
+				if (userNum == randomNum) {
+						$('#userNum').html(userNum);
+						wins++;
+						$('#wins').html("Wins: " + wins);
+						// alert
+						alert("You Win!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+
+				// if user's number is greater than random number, add 1 point to wins in html
+				} else if (userNum > randomNum) {
+						$('#userNum').html(userNum);
+						losses++;
+						$('#losses').html("Losses: " + losses);
+						// alert
+						alert("You Lose!");
+
+						// reset buttons and userNum 
+						userNum = 0;
+						gem1(); gem2(); gem3(); gem4();
+				}
 			})
 //===============================================================================
-
-// USER NUMBER
-
-
-// add each number in 'valueArray'
-for (var i = 0; i < valueArray.length; i++) {
-    userNum += valueArray[i]; 
-    console.log(userNum);
-}
 
 
 });
