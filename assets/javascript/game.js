d3a800ba101e30aa033932065b6015b78b60d94c
Tabitha Love <tabinylove@tabithas-mbp.uconnect.utah.edu>
2017-07-12 18:17:35 -0600

    condensed version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 	var userNum; // number the user adds together to reach random number
 	var randomValue; // random value assigned to the buttons (between 1-12)
 	var parseValue; // the 'randomValue' in integer form
+	var gemButtons; // var to hold the buttons from html class
 	var wins = 0; // number of wins
 	var losses = 0; // number of losses
 //===============================================================================
@@ -35,185 +36,6 @@ $(document).ready(function(){
 
 //===============================================================================
 
-// BUTTONS
-
-/* set a random number between 1 and 10 that will be used 
-to determine the value of the crystals.*/
-
-	randomValue = Math.floor(Math.random() * (10 - 1) + 1);
-
-/*-------This ^^^ and the buttons are repeated so that randomValue will NOT be set as the
-same number for all buttons. (i.e. don't use '.btn' instead of '#gem1', ect.)-------*/
-
-	// FIRST
-	// set the value attribute on the buttons as the randomValue
-	$("#gem1").attr("value", randomValue);
-	console.log("#1: " + randomValue);
-
-		// on click, parse the value and add it to 'userNum'
-		$("#gem1").on("click", function gem1() {
-				// parseInt the value
-				parseValue = parseInt($(this).attr("value"));
-				// add the value to the userNum
-				userNum = userNum + parseValue;
-				console.log(userNum);
-				// show the result in html
-				$('#userNum').html(userNum);
-
-				// WIN / LOSE
-
-				// if the user's number matches the random number, add 1 point to wins in html
-				if (userNum == randomNum) {
-						$('#userNum').html(userNum);
-						wins++;
-						$('#wins').html("Wins: " + wins);
-						// alert
-						alert("You Win!");
-
-						// reset buttons and userNum 
-						reset();
-
-				// if user's number is greater than random number, add 1 point to wins in html
-				} else if (userNum > randomNum) {
-						$('#userNum').html(userNum);
-						losses++;
-						$('#losses').html("Losses: " + losses);
-						// alert
-						alert("You Lose!");
-
-						// reset buttons and userNum 
-						reset();
-				}
-
-		})
-
-	// SECOND
-	// set the value attribute on the buttons as the randomValue
-	randomValue = Math.floor(Math.random() * (10 - 1) + 1);
-	$("#gem2").attr("value", randomValue);
-	console.log("#2: " + randomValue);
-
-		// on click, parse the value and add it to 'userNum'
-			$("#gem2").on("click", function gem2() {
-				// parseInt the value
-				parseValue = parseInt($(this).attr("value"));
-				// add the value to the userNum
-				userNum = userNum + parseValue;
-				console.log(userNum);
-				// show the result in html
-				$('#userNum').html(userNum);
-
-				// WIN / LOSE
-
-				// if the user's number matches the random number, add 1 point to wins in html
-				if (userNum == randomNum) {
-						$('#userNum').html(userNum);
-						wins++;
-						$('#wins').html("Wins: " + wins);
-						// alert
-						alert("You Win!");
-
-						// reset buttons and userNum 
-						reset();
-
-				// if user's number is greater than random number, add 1 point to wins in html
-				} else if (userNum > randomNum) {
-						$('#userNum').html(userNum);
-						losses++;
-						$('#losses').html("Losses: " + losses);
-						// alert
-						alert("You Lose!");
-
-						// reset buttons and userNum 
-						reset();
-				}
-		})
-
-	// THIRD
-	// set the value attribute on the buttons as the randomValue
-	randomValue = Math.floor(Math.random() * (10 - 1) + 1);
-	$("#gem3").attr("value", randomValue);
-	console.log("#3: " + randomValue);
-
-		// on click, parse the value and add it to 'userNum'
-			$("#gem3").on("click", function gem3() {
-				// parseInt the value
-				parseValue = parseInt($(this).attr("value"));
-				// add the value to the userNum
-				userNum = userNum + parseValue;
-				console.log(userNum);
-				// show the result in html
-				$('#userNum').html(userNum);
-
-				// WIN / LOSE
-
-				// if the user's number matches the random number, add 1 point to wins in html
-				if (userNum == randomNum) {
-						$('#userNum').html(userNum);
-						wins++;
-						$('#wins').html("Wins: " + wins);
-						// alert
-						alert("You Win!");
-
-						// reset buttons and userNum 
-						reset();
-
-				// if user's number is greater than random number, add 1 point to wins in html
-				} else if (userNum > randomNum) {
-						$('#userNum').html(userNum);
-						losses++;
-						$('#losses').html("Losses: " + losses);
-						// alert
-						alert("You Lose!");
-
-						// reset buttons and userNum 
-						reset();
-				}
-		})
-
-	// FOURTH
-	// set the value attribute on the buttons as the randomValue
-	randomValue = Math.floor(Math.random() * (10 - 1) + 1);
-	$("#gem4").attr("value", randomValue);
-	console.log("#4: " + randomValue);
-
-		// on click, parse the value and add it to 'userNum'
-			$("#gem4").on("click", function gem4() {
-				// parseInt the value
-				parseValue = parseInt($(this).attr("value"));
-				// add the value to the userNum
-				userNum = userNum + parseValue;
-				console.log(userNum);
-				// show the result in html
-				$('#userNum').html(userNum);
-
-				// WIN / LOSE
-
-				// if the user's number matches the random number, add 1 point to wins in html
-				if (userNum == randomNum) {
-						$('#userNum').html(userNum);
-						wins++;
-						$('#wins').html("Wins: " + wins);
-						// alert
-						alert("You Win!");
-
-						// reset buttons and userNum 
-						reset();
-
-				// if user's number is greater than random number, add 1 point to wins in html
-				} else if (userNum > randomNum) {
-						$('#userNum').html(userNum);
-						losses++;
-						$('#losses').html("Losses: " + losses);
-						// alert
-						alert("You Lose!");
-
-						// reset buttons and userNum 
-						reset();
-				}
-			})
-//===============================================================================
-
 // RESET
 
 function reset() {
@@ -224,5 +46,61 @@ function reset() {
 		gem1(); gem2(); gem3(); gem4();
 
 }
+//===============================================================================
+
+// WIN / LOSE
+
+	function winLose () {
+
+		// if the user's number matches the random number, add 1 point to wins in html
+		if (userNum == randomNum) {
+				$('#userNum').html(userNum);
+				wins++;
+				$('#wins').html("Wins: " + wins);
+				// alert
+				alert("You Win!");
+
+				// reset buttons and userNum 
+				reset();
+
+		// if user's number is greater than random number, add 1 point to wins in html
+		} else if (userNum > randomNum) {
+				$('#userNum').html(userNum);
+				losses++;
+				$('#losses').html("Losses: " + losses);
+				// alert
+				alert("You Lose!");
+
+				// reset buttons and userNum 
+				reset();
+		}
+	}
+//===============================================================================
+
+// BUTTONS
+
+	// grab all buttons from document using a class
+	gemButtons = document.getElementsByClassName('btn');
+
+	// for each button, assign a random value
+	for (var i = 0; i < gemButtons.length; i++) {
+			randomValue = Math.floor(Math.random() * (10 - 1) + 1);
+			gemButtons[i].setAttribute("value", randomValue);
+	}
+	
+	
+	// on click, parse the random value and add it to 'userNum'
+	$(".btn").on("click", function gem1() {
+			// parseInt the value
+			parseValue = parseInt($(this).attr("value"));
+			// add the value to the userNum
+			userNum = userNum + parseValue;
+			console.log(userNum);
+			// show the result in html
+			$('#userNum').html(userNum);
+
+			// call the win or lose function
+			winLose();
+			})
 
 });
